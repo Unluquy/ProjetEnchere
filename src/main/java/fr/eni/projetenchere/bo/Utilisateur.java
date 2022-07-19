b90ec31f8c6b62b4836c5256867f4095db292359
Unluquy <goreckinathans@gmail.com>
2022-07-19 11:58:20 +0200

    Start on the connect.jsp page; Linked connect.jsp to the Servlet

---
+++ src/main/java/fr/eni/projetenchere/bo/Utilisateur.java
@@ -0,0 +1,5 @@
+package fr.eni.projetenchere.bo;
+
+public class Utilisateur {
+    private int o_utilisateur;
+}
---
+++ src/main/java/fr/eni/projetenchere/servlet/ServletConnection.java
@@ -0,0 +1,26 @@
+package fr.eni.projetenchere.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletConnection", value = "/ServletConnection")
+public class ServletConnection extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1> Connection </h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
