bc34abdfbcf176499bbecf671367e1bee2607ce7
Unluquy <goreckinathans@gmail.com>
2022-07-22 15:09:13 +0200

    created / corrected Bo

---
+++ src/main/java/fr/eni/projetenchere/bo/ArticleVendu.java
@@ -0,0 +1,103 @@
+package fr.eni.projetenchere.bo;
+
+import java.util.Date;
+
+public class ArticleVendu {
+
+    private int noAticle;
+    private String nomArticle;
+    private String description;
+    private Date dateDebutEncheres;
+    private Date dateFinEncheres;
+    private int prixInitial;
+    private int prixVente;
+    private Utilisateur user;
+    private Categorie categorie;
+
+    public int getNoAticle() {
+        return noAticle;
+    }
+
+    public void setNoAticle(int noAticle) {
+        this.noAticle = noAticle;
+    }
+
+    public String getNomArticle() {
+        return nomArticle;
+    }
+
+    public void setNomArticle(String nomArticle) {
+        this.nomArticle = nomArticle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateDebutEncheres() {
+        return dateDebutEncheres;
+    }
+
+    public void setDateDebutEncheres(Date dateDebutEncheres) {
+        this.dateDebutEncheres = dateDebutEncheres;
+    }
+
+    public Date getDateFinEncheres() {
+        return dateFinEncheres;
+    }
+
+    public void setDateFinEncheres(Date dateFinEncheres) {
+        this.dateFinEncheres = dateFinEncheres;
+    }
+
+    public int getPrixInitial() {
+        return prixInitial;
+    }
+
+    public void setPrixInitial(int prixInitial) {
+        this.prixInitial = prixInitial;
+    }
+
+    public int getPrixVente() {
+        return prixVente;
+    }
+
+    public void setPrixVente(int prixVente) {
+        this.prixVente = prixVente;
+    }
+
+    public Utilisateur getUser() {
+        return user;
+    }
+
+    public void setUser(Utilisateur user) {
+        this.user = user;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public ArticleVendu() {
+    }
+
+    public ArticleVendu(int noAticle, String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres, int prixInitial, int prixVente, Utilisateur user, Categorie categorie) {
+        this.noAticle = noAticle;
+        this.nomArticle = nomArticle;
+        this.description = description;
+        this.dateDebutEncheres = dateDebutEncheres;
+        this.dateFinEncheres = dateFinEncheres;
+        this.prixInitial = prixInitial;
+        this.prixVente = prixVente;
+        this.user = user;
+        this.categorie = categorie;
+    }
+}
---
+++ src/main/java/fr/eni/projetenchere/bo/Categorie.java
@@ -0,0 +1,31 @@
+package fr.eni.projetenchere.bo;
+
+public class Categorie {
+    private int noCategorie;
+    private String libelle;
+
+    public int getNoCategorie() {
+        return noCategorie;
+    }
+
+    public void setNoCategorie(int noCategorie) {
+        this.noCategorie = noCategorie;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public Categorie() {
+    }
+
+    public Categorie(int noCategorie, String libelle) {
+        this.noCategorie = noCategorie;
+        this.libelle = libelle;
+    }
+
+}
